6d99b6bd7f9005e163eeb70a6f7cfae1e988ab60
nab-circ <125284596+nab-circ@users.noreply.github.com>
2023-02-14 11:16:44 +0100

    minor updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,13 @@ import streamlit as st
 from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource
 
-sohdf = pd.read_csv('sohdata.csv')
+sohdf = pd.read_csv('sohdata.csv', parse_dates=['Date'])
 st.line_chart(sohdf, x="Date", y="SoH")
 
 sohdf_b = pd.read_csv('sohdata.csv', parse_dates=['Date'])
 source = ColumnDataSource(sohdf_b)
 
-p = figure(x_axis_type="datetime", x_axis_label='Time', y_axis_label='SoH(%)')
+p = figure(x_axis_type="datetime", x_axis_label='Time', y_axis_label='SoH(%)', y_range=(70, 100), tools=['pan', 'wheel_zoom'])
 p.line(x='Date', y='SoH', line_width=2, source=source)
 
 p.toolbar.logo = None
@@ -20,7 +20,7 @@ hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
         footer {visibility: hidden;}
-        .viewerBadge_container__1QSob {visibility: hidden;}
+        a {visibility: hidden;}
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
