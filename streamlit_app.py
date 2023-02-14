bb8ec7e709aeffb807bac911dd73ce26a85a7966
nab-circ <125284596+nab-circ@users.noreply.github.com>
2023-02-14 11:20:07 +0100

    minor

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,11 +9,11 @@ st.line_chart(sohdf, x="Date", y="SoH")
 sohdf_b = pd.read_csv('sohdata.csv', parse_dates=['Date'])
 source = ColumnDataSource(sohdf_b)
 
-p = figure(x_axis_type="datetime", x_axis_label='Time', y_axis_label='SoH(%)', y_range=(70, 100), tools=['pan', 'wheel_zoom'])
+p = figure(x_axis_type="datetime", x_axis_label='Time', y_axis_label='SoH(%)', y_range=(70, 100), tools=['pan', 'wheel_zoom', "box_zoom", "reset"], plot_width=400, plot_height=200)
 p.line(x='Date', y='SoH', line_width=2, source=source)
 
 p.toolbar.logo = None
-p.toolbar_location = None
+# p.toolbar_location = None
 st.bokeh_chart(p, use_container_width=True)
 
 hide_menu_style = """
