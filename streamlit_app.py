45a49e641da5e32769461605318be2ff6827b56f
nab-circ <125284596+nab-circ@users.noreply.github.com>
2023-02-14 10:31:56 +0100

    minor update

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.line_chart(sohdf, x="Date", y="SoH")
 hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
-        footer {visibility: hidden;}
+        #footer {visibility: hidden;}
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
