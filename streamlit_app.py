805fd57111be6c7bda546b163f16ffee5e994c5c
nab-circ <125284596+nab-circ@users.noreply.github.com>
2023-02-14 11:26:23 +0100

    limit scrolling

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,9 @@ sohdf_b = pd.read_csv('sohdata.csv', parse_dates=['Date'])
 source = ColumnDataSource(sohdf_b)
 
 p = figure(x_axis_type="datetime", x_axis_label='Time', y_axis_label='SoH(%)', y_range=(70, 100), tools=['pan', 'wheel_zoom', "box_zoom", "reset"], plot_width=400, plot_height=200)
+p.x_range.bounds=(min(sohdf_b['Date']),max(sohdf_b['Date']))
+p.y_range.bounds=(0,100)
+
 p.line(x='Date', y='SoH', line_width=2, source=source)
 
 p.toolbar.logo = None
